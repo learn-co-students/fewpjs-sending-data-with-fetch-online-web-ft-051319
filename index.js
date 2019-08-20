9a50a40d823ee135fb355ea1e427c7bc12e44b51
Brian Velez <bvez94@gmail.com>
2019-08-20 09:24:56 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your code here
+const destinationURL = "http://localhost:3000/dogs";
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        dogName: "Byron",
+        dogBreed: "Poodle"
+    })
+};
+
+fetch(destinationURL, configurationObject)
+.then(response => response.json())
+.then(object => console.log(object));
+
+function submitData(userName, userEmail) {
+    let url = "http://localhost:3000/users";
+    let configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    }
+    let body = document.querySelector("body");
+    return fetch(url, configObject)
+    .then(response => response.json())
+    .then(object => body.innerHTML = object.id)
+    .catch(function(error){
+        alert("Error detected!");
+        body.innerHTML = error.message;
+    });
+};
