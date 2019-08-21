8383c37a8c4bef942a9bc164a1c2e8d84f40f0c1
haleydek <hcd09@live.com>
2019-08-21 08:53:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Add your code here
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+   
+//   let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+   
+//   fetch("http://localhost:3000/dogs", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     });
+
+function createConfigObj(name, email) {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    }
+    return configObj
+}
+
+function submitData(name, email) {
+    let configObject = createConfigObj(name, email);
+
+    return fetch("http://localhost:3000/users", configObject)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(userObject) {
+            let body = document.querySelector("body script")
+
+            body.innerHTML += `<p>${userObject.id}</p>`
+            // let p = document.createElement("p")
+
+            // p.innerText = `${userObject["id"]}`
+            // body.appendChild(p)
+        })
+        .catch(function(error) {
+            let body = document.querySelector("body script")
+
+            body.innerHTML += `<p>${error.message}</p>`
+        })
+}
