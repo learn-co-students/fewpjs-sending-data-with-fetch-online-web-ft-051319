a8c5d0c9eec26b610d07b971ef2c790838816ce1
Brian <briandsalemi@gmail.com>
2019-08-21 21:00:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+     .then(function(response) {
+        return response.json();
+       })
+       .then(function(object) {
+           let bodyNode = document.querySelector('body');
+           let paragraph = document.createElement('p');
+           paragraph.innerHTML = `${object.id}`;
+           bodyNode.append(paragraph);
+       })
+       .catch(function(error) {
+        let bodyNode = document.querySelector('body');
+        let paragraph = document.createElement('p');
+        alert("Unauthorized Access");
+        paragraph.innerHTML = error.message;
+        bodyNode.append(paragraph)
+      });
+};
+
+submitData();
