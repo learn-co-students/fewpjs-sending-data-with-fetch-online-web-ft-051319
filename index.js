a4cc609d1248f917eb42583d790dfe2d2e1d012b
Natalie <natsetc@gmail.com>
2019-08-28 12:58:00 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({name: name, email: email})
+
+        // can also be abstracted
+        // body: JSON.stringify({name, email})
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object.id
+    })
+    .catch(function(error) {
+        alert("Unauthorized Access");
+        document.body.innerHTML = error.message
+    });
+}
