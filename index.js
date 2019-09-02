b147c1f0cbfc62fae7347d6c5df90aa5acf76266
Kane Koo <kanekoo@gmail.com>
2019-09-02 17:25:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+function submitData(pname, pemail) {
+  let configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: `${pname}`,
+      email: `${pemail}`
+    })
+
+  };
+  let bodyNode=document.querySelector("body");
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(resp => resp.json())
+    .then(json => {
+        bodyNode.innerHTML += `${json.id} ${json.name} ${json.email}<br>`
+    })
+    .catch(function(error) {
+      alert("Unauthorized access");
+      bodyNode.innerHTML +=  `${error.message}<br>`
+    })
+}
